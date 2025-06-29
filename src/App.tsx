6d4ad47aedbfe61cb830961612d7c1ba2c4c25f7
WeslleySORDev <weslleysordev@gmail.com>
2025-06-29 17:36:13 -0300

    feat: static header create

--- src/App.tsx
+++ src/App.tsx
@@ -50,88 +50,113 @@ export default function App() {
     console.log(values);
   }
   return (
-    <main className="max-w-[640px] h-screen px-[30px] py-8 border border-zinc-300 mx-auto">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col h-full"
-        >
-          <div className="flex flex-1 pb-[42px] pt-[32px] border-y border-[#E5E7EB] items-start w-full">
-            <div className="grid grid-cols-1 gap-y-6 gap-x-10 w-full sm:grid-cols-2">
-              <FormField
-                control={form.control}
-                name="fullName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome Completo</FormLabel>
-                    <FormControl>
-                      <Input placeholder="ex: João da Silva" {...field} />
-                    </FormControl>
-                    <FormDescription className="sr-only">
-                      Aqui você colocará seu nome completo.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="emailAddress"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="name@email.com" {...field} />
-                    </FormControl>
-                    <FormDescription className="sr-only">
-                      Aqui você colocará seu email.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="phoneNumber"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Número de telefone</FormLabel>
-                    <FormControl>
-                      <Input placeholder="24999999999" {...field} />
-                    </FormControl>
-                    <FormDescription className="sr-only">
-                      Aqui você colocará seu número de telefone.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="portfolio"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Portifólio/Github Link</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="github.com/weslleysordev"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription className="sr-only">
-                      Aqui você colocará seu portifolio ou github.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+    <div className="flex flex-col max-w-[640px] h-screen px-[30px] py-8 border border-zinc-300 mx-auto">
+      <header className="flex gap-[18px] justify-center items-center mb-8">
+        <div className="flex items-center justify-center w-[34px] h-[34px] rounded-full bg-[#FC6C4C]">
+          <span className="font-medium text-white">1</span>
+        </div>
+        <div className="flex items-center gap-[18px]">
+          <div className="w-[100px] h-1.5 rounded-[50px] bg-[#E5E7EB]"></div>
+          <div className="flex items-center justify-center w-[34px] h-[34px] rounded-full bg-[#E5E7EB]">
+            <span className="font-medium">2</span>
           </div>
-          <Button className="mt-auto cursor-pointer" type="submit">
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </main>
+        </div>
+        <div className="flex items-center gap-[18px]">
+          <div className="w-[100px] h-1.5 rounded-[50px] bg-[#E5E7EB]"></div>
+          <div className="flex items-center justify-center w-[34px] h-[34px] rounded-full bg-[#E5E7EB]">
+            <span className="font-medium">3</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-[18px]">
+          <div className="w-[100px] h-1.5 rounded-[50px] bg-[#E5E7EB]"></div>
+          <div className="flex items-center justify-center w-[34px] h-[34px] rounded-full bg-[#E5E7EB]">
+            <span className="font-medium">4</span>
+          </div>
+        </div>
+      </header>
+      <main className="h-full">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col h-full"
+          >
+            <div className="flex flex-1 pb-[42px] pt-[32px] border-y border-[#E5E7EB] items-start w-full">
+              <div className="grid grid-cols-1 gap-y-6 gap-x-10 w-full sm:grid-cols-2">
+                <FormField
+                  control={form.control}
+                  name="fullName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nome Completo</FormLabel>
+                      <FormControl>
+                        <Input placeholder="ex: João da Silva" {...field} />
+                      </FormControl>
+                      <FormDescription className="sr-only">
+                        Aqui você colocará seu nome completo.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="emailAddress"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="name@email.com" {...field} />
+                      </FormControl>
+                      <FormDescription className="sr-only">
+                        Aqui você colocará seu email.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phoneNumber"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Número de telefone</FormLabel>
+                      <FormControl>
+                        <Input placeholder="24999999999" {...field} />
+                      </FormControl>
+                      <FormDescription className="sr-only">
+                        Aqui você colocará seu número de telefone.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="portfolio"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Portifólio/Github Link</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="github.com/weslleysordev"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription className="sr-only">
+                        Aqui você colocará seu portifolio ou github.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <Button className="mt-auto cursor-pointer" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </main>
+    </div>
   );
 }
